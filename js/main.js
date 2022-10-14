530f600e1c527c24553bf6568a62458c1b52d6a4
Lisa Jin <l.jin1@EDU.epsb.ca>
2022-10-14 14:49:08 -0600

    waaa

--- js/main.js
+++ js/main.js
@@ -4,7 +4,7 @@
 let signInBtn = document.getElementById('sign-in-btn');
 let signUpBtn = document.getElementById('sign-up-btn');
 
-let users = saveUser();
+let users = loadUsers();
 
 // SIGN UP BTN CLICKED
 signUpBtn.addEventListener('click', signUpHandler);
@@ -14,14 +14,16 @@ function signUpHandler() {
   let userInp = document.getElementById("signUser").value;
   let passInp = document.getElementById("signPass").value;
   let passCfm = document.getElementById("signCfm").value;
-  if (passInp === passCfm) {
-    console.log("User Saved");
-    users.push(newUser(userInp, passInp))
-    saveUser();
-  } else {
-    console.log("Pass no match");
+  // for (let i = 0; i < users.length; i++) {
+    if (passInp === passCfm) {
+      console.log("User Saved");
+      users.push(newUser(userInp, passInp))
+      saveUser();
+    } else {
+      console.log("Pass no match");
+    }
   }
-}
+// }
 
 function newUser(username, password) {
   return {
@@ -38,8 +40,8 @@ function signInHandler() {
   let userAtmp = document.getElementById("logUser").value;
   let passAtmp = document.getElementById("logPass").value;
   for (let i = 0; i < users.length; i++) {
-    if (userAtmp === users.name && passAtmp === users.pass) {
-      console.log();
+    if (userAtmp === users[i].name && passAtmp === users[i].pass) {
+      console.log("ye");
     } else {
       console.log("no");
     }
@@ -48,4 +50,9 @@ function signInHandler() {
 
 function saveUser() {
   localStorage.setItem("users", JSON.stringify(users));
+}
+
+function loadUsers() {
+  let usersStr = localStorage.getItem("users");
+  return JSON.parse(usersStr) ?? [];
 }
