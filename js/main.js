baf737c3e9c1882359fb1ba141c3c30af347b0f2
Lisa Jin <l.jin1@EDU.epsb.ca>
2022-12-06 14:43:48 -0700

    doesd

--- js/main.js
+++ js/main.js
@@ -11,13 +11,11 @@ saveUser();
 signUpBtn.addEventListener('click', signUpHandler);
 
 function signUpHandler() {
-  console.log('Sign Up Btn Clicked');
   let userInp = document.getElementById("signUser").value;
   let passInp = document.getElementById("signPass").value;
   let passCfm = document.getElementById("signCfm").value;
   if (passInp === passCfm) {
     let found = userFound(userInp);
-    console.log(found);
     if (found === 1) {
       alert("User Added");
       users.push(newUser(userInp, passInp));
@@ -45,13 +43,23 @@ function signInHandler() {
   let passAtmp = document.getElementById("logPass").value;
 
   for (let i = 0; i < users.length; i++) {
-    if (userAtmp === users[i].name && passAtmp === users[i].pass) {
+    if (passFound(passAtmp, userAtmp) === -1) {
       alert("Sign in Successful");
       break;
     } else {
       alert("Sign in Unsuccessful");
+      break;
     }
   }
+}
+
+function passFound(passTest, userTest) {
+  for (let i = 0; i < users.length; i++) {
+    if (passTest === users[i].pass && userTest === users[i].name) {
+      return -1;
+    }
+  }
+  return 1;
 }
 
 function userFound(userTest) {
@@ -63,7 +71,6 @@ function userFound(userTest) {
   return 1;
 }
 
-
 function saveUser() {
   localStorage.setItem("users", JSON.stringify(users));
 }
@@ -72,5 +79,3 @@ function loadUsers() {
   let usersStr = localStorage.getItem("users");
   return JSON.parse(usersStr) ?? [];
 }
-
-// 2201
